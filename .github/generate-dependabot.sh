2fec54c2b817ff7b6ce16a442e81a310d4912de4
Don Bower <DonBower@users.noreply.github.com>
2023-08-15 12:09:02 -0700

    0.1.0 initial release (#1)
    * Initial Release
    
    * more cleanup
    
    * Initial commit
    
    * Add Issues badge
    
    * update badge size
    
    * Order and Spacing

---
+++ .github/generate-dependabot.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+set -euo pipefail
+
+dependabot_file=.github/dependabot.yml
+
+# Get a list of Terraform examples.
+all_terraform_examples=$(find . -type f -name '*.tf' | sed 's#/[^/]*$##' | sed 's/.\///' | sort | uniq)
+
+# Write the dependabot configuation file.
+echo "> Generating the .github/dependabot.yml configuration file..."
+cat >$dependabot_file <<EOL
+---
+# This file is auto-generated. Do not manually amend the configuration.
+# https://github.com/DonBower/packer-vsphere-ubuntu/blob/main/.github/generate-dependabot.sh
+
+version: 2
+updates:
+  - package-ecosystem: "github-actions"
+    directory: "/"
+    schedule:
+      interval: "daily"
+  # Dependabot does not currently support wildcard or multiple directory declarations.
+  # https://github.com/dependabot/dependabot-core/issues/2178
+EOL
+
+for example in $all_terraform_examples; do
+	{
+		echo '  - package-ecosystem: "terraform"'
+		echo "    directory: \"/$example\""
+		echo "    schedule:"
+		echo '      interval: "daily"'
+	} >>$dependabot_file
+done
+
+echo "> The .github/dependabot.yml file has been generated."
+echo ""
+
+echo "> Results:"
+echo ""
+cat $dependabot_file
---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Release History
+
+## .Next
+
+> Release Date: Unreleased
+
+:boom: **Enhancement**:
+
+- Initial Release
+
+:sweat_drops: **Chore**:
+
+- Updates Remove extra code, clean up.
